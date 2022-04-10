48053a6c09e6c885d577a21680aaf3e6dd4d0388
giornotheme <vtang@outlook.fr>
2022-04-10 13:29:19 +0200

    commit chiffrement vigenere

--- vigenere.py
+++ vigenere.py
@@ -1,10 +1,11 @@
-def chiffrement(mdp : str, liste : list, cle : int) -> str:
+def chiffrement(mdp : str, liste : list) -> str:
     mot_de_passe_cryptée=""
     i = 0
+    cle = 0
     while i != len(mdp):
         o = 0
         emplacement_lettre = liste.index(mdp[i])
-
+        cle = emplacement_lettre
         while o != cle:
             if emplacement_lettre == liste.index(liste[-1]):
                 emplacement_lettre = liste.index(liste[0])
@@ -15,8 +16,9 @@ def chiffrement(mdp : str, liste : list, cle : int) -> str:
         mot_de_passe_cryptée = mot_de_passe_cryptée + liste[emplacement_lettre]
         i = i + 1
     return mot_de_passe_cryptée
-mot_de_passe = input("entrez votre mot de_passe") 
-cle = int(input("entrez votre cle de cesar"))
+
+
+mot_de_passe = input("entrez votre mot de_passe\n") 
 liste = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-print(chiffrement(mot_de_passe, liste, cle))
+print(chiffrement(mot_de_passe, liste))
